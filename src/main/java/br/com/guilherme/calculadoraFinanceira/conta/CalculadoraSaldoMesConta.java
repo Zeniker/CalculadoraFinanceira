a2b6ae66d0e66cb9d092a4dfcd6d015cdc36a75a
Guilherme Lopes <gui.lls@hotmail.com>
2019-08-13 09:58:58 -0300

    Criada calculadora de saldo da conta.

---
+++ src/main/java/br/com/guilherme/calculadoraFinanceira/conta/CalculadoraSaldoMesConta.java
@@ -0,0 +1,35 @@
+package br.com.guilherme.calculadoraFinanceira.conta;
+
+import br.com.guilherme.calculadoraFinanceira.movimentacao.Movimentacao;
+import br.com.guilherme.calculadoraFinanceira.movimentacao.MovimentacaoRepository;
+
+import java.math.BigDecimal;
+import java.time.Month;
+import java.util.List;
+
+/**
+ * Calculadora de saldo de uma conta
+ */
+//@Service
+public class CalculadoraSaldoMesConta {
+
+    private final MovimentacaoRepository movimentacaoRepository;
+
+    public CalculadoraSaldoMesConta(MovimentacaoRepository movimentacaoRepository){
+
+        this.movimentacaoRepository = movimentacaoRepository;
+    }
+
+    /**
+     * @param conta Conta que deve ser calculado o saldo
+     * @param month Mês relativo do saldo
+     * @return
+     */
+    public BigDecimal calcula(Conta conta, Month month){
+        List<Movimentacao> movimentacoes = movimentacaoRepository.getMovimentacaoByContaAndDataCobrancaMonthAndStatus(conta,
+                month.getValue(),
+                Movimentacao.StatusMovimentacao.ATIVO);
+
+        return movimentacoes.stream().map(Movimentacao::getSaldo).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
---
+++ src/test/java/br/com/guilherme/calculadoraFinanceira/conta/CalculadoraSaldoMesContaTest.java
@@ -0,0 +1,66 @@
+package br.com.guilherme.calculadoraFinanceira.conta;
+
+import br.com.guilherme.calculadoraFinanceira.movimentacao.Movimentacao;
+import br.com.guilherme.calculadoraFinanceira.movimentacao.MovimentacaoRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class CalculadoraSaldoMesContaTest {
+
+    private Conta conta;
+
+    @Autowired
+    private ContaRepository contaRepository;
+
+    @Autowired
+    private MovimentacaoRepository movimentacaoRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        conta = new Conta();
+        conta.setNomePessoa("Jorge");
+
+        contaRepository.save(conta);
+    }
+
+    private void incluiNovaMovimentacao(LocalDate data, BigDecimal valor){
+        Movimentacao mov = new Movimentacao();
+        mov.setConta(conta);
+        mov.setRecorrente(false);
+        mov.setStatus(Movimentacao.StatusMovimentacao.ATIVO);
+        mov.setDataCobranca(data);
+        mov.setDataOperacao(data);
+        mov.setValorMovimentacao(valor);
+        mov.setTipoMovimentacao(Movimentacao.TipoMovimentacao.GANHO);
+
+        movimentacaoRepository.save(mov);
+    }
+
+
+    @Test
+    public void calculaSaldoMes() {
+        incluiNovaMovimentacao(LocalDate.now(), new BigDecimal("150"));
+        incluiNovaMovimentacao(LocalDate.now(), new BigDecimal("200"));
+        incluiNovaMovimentacao(LocalDate.now().plusMonths(1), new BigDecimal("200"));
+
+        CalculadoraSaldoMesConta calculadoraSaldoMesConta = new CalculadoraSaldoMesConta(movimentacaoRepository);
+        BigDecimal saldo = calculadoraSaldoMesConta.calcula(conta, LocalDate.now().getMonth());
+
+        assertEquals(new BigDecimal("350.00"), saldo);
+    }
+
+
+}
