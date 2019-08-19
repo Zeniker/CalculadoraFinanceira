bd8c870658473df5408a36f660ee60131e62882e
Guilherme Lopes <gui.lls@hotmail.com>
2019-08-19 08:31:49 -0300

    Alterados enums da movimentação. Alterado para incluir movimentações padrão.

--- src/main/java/br/com/guilherme/calculadoraFinanceira/movimentacao/Movimentacao.java
+++ src/main/java/br/com/guilherme/calculadoraFinanceira/movimentacao/Movimentacao.java
@@ -4,10 +4,7 @@ import br.com.guilherme.calculadoraFinanceira.cartao.Cartao;
 import br.com.guilherme.calculadoraFinanceira.conta.Conta;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -35,6 +32,7 @@ public class Movimentacao {
     private boolean recorrente;
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     private StatusMovimentacao status;
 
     @NotNull
@@ -48,6 +46,7 @@ public class Movimentacao {
     private BigDecimal valor;
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     private TipoMovimentacao tipoMovimentacao;
 
     public enum StatusMovimentacao {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
 insert into conta(id, nome_pessoa) values (1, 'Guilherme');
+
+insert into movimentacao (id, data_cobranca, data_operacao, recorrente, status, tipo_movimentacao, valor, cartao_id, conta_id, original_id)
+values(1, '2019-08-13', '2019-08-13', false, 'ATIVO', 'GANHO', 10, null, 1, null);
+
+insert into movimentacao (id, data_cobranca, data_operacao, recorrente, status, tipo_movimentacao, valor, cartao_id, conta_id, original_id)
+values(2, '2019-08-13', '2019-08-13', false, 'ATIVO', 'GANHO', 100, null, 1, null);
