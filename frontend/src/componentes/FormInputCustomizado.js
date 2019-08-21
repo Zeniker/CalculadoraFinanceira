1497c2a70f01295d4b3751b1c8d738f0723ffb1c
Guilherme Lopes <gui.lls@hotmail.com>
2019-08-21 08:29:24 -0300

    Criadas funções separadas para subscribe para facilitar entendimento.

--- frontend/src/componentes/FormInputCustomizado.js
+++ frontend/src/componentes/FormInputCustomizado.js
@@ -15,12 +15,19 @@ class FormInputCustomizado extends Component{
   }
 
   componentDidMount(){
+    this.subscribeMensagemErro();
+    this.subscribeLimpaErros();
+  }
+
+  subscribeMensagemErro(){
     PubSub.subscribe("erro-validacao", function (topico, erro) {
       if(erro.field === this.props.id){
         this.setState({msgErro: erro.defaultMessage});
       }
     }.bind(this));
+  }
 
+  subscribeLimpaErros(){
     PubSub.subscribe("limpar-erros-validacao", function(topico, erro){
       this.setState({msgErro: ''});
     }.bind(this));
